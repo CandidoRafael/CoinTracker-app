dc40e053c73769fd2f7bf524ce00534535ea05d1
Rafael Adelino Candido <112955398+CandidoRafael@users.noreply.github.com>
2023-08-09 14:31:18 -0300

    Update firebase-config.js

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -2,12 +2,12 @@ import firebase from "firebase/compat/app"
 import "firebase/compat/auth"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBoILqFY0XbEEUKn9OUQLZnNySTPIzFdto",
-    authDomain: "cointrackr-2ca3d.firebaseapp.com",
-    projectId: "cointrackr-2ca3d",
-    storageBucket: "cointrackr-2ca3d.appspot.com",
-    messagingSenderId: "598095200031",
-    appId: "1:598095200031:web:f2555d9c3c56343681fa2a"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
   firebase.initializeApp(firebaseConfig)
